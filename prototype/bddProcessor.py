78f0904cde38a8158c930b157ca6d92487cd2184
Anders Larsen <anders.l.h.larsen@live.dk>
2024-03-10 14:50:34 +0100

    fixed grammar

--- prototype/bddProcessor.py
+++ prototype/bddProcessor.py
@@ -8,37 +8,16 @@ with open(os.path.join(dirname, 'grammar.lark'), 'r') as file:
 
 dsl_parser = Lark(dsl_grammar, start='start')
 
-text = '''
-model vending system
-
-entity vendingMachine {
-  actions: insert, select, pick, cancel
-  states: idle, dispensingMode, selectionMode, extraDispensingMode
-  properties: option, insertedAmount, product
-}
-
-entity product {
-  states: returned
-}
-
-entity inserted amount {
-  states: returned
-}
-
-Scenario: Waiting_for_drink_selection
-Given the vendingMachine is idle
-When I insert $2
-Then the vendingMachine is in selectionMode
-'''
-
 def parse (text):
     return dsl_parser.parse(text)
 
 # ast = dsl_parser.parse(text)
-# if __name__ == "__main__":
-#     print(ast.pretty())
-#     print("\n")
-#     for sub in ast.iter_subtrees_topdown():
-#         print(sub.data)
-#         # print("\n")
+if __name__ == "__main__":
+    text = open(os.path.join(dirname, "BDD.text"), "r")
+    ast = parse(text.read())
+    print(ast.pretty())
+    print("\n")
+    for sub in ast.iter_subtrees_topdown():
+        print(sub)
+        print("\n")
 
